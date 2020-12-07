7a36e5f908b8dade5cc58f5b53c19293f012e774
Fabiano Guimarães <fabio.dub@outlook.com>
2020-12-07 20:16:21 -0300

    Referencia de posts em user

--- src/main/java/com/soft/config/Instanciacao.java
+++ src/main/java/com/soft/config/Instanciacao.java
@@ -18,7 +18,7 @@ import com.soft.repository.UserRepository;
 public class Instanciacao implements CommandLineRunner{
 	
 	@Autowired
-	private UserRepository repository;
+	private UserRepository userRepository;
 	
 	@Autowired
 	private PostRepository postRepository;
@@ -29,19 +29,23 @@ public class Instanciacao implements CommandLineRunner{
 		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
 		sdf.setTimeZone(TimeZone.getTimeZone("GMT"));
 		
-		repository.deleteAll();
+		userRepository.deleteAll();
 		postRepository.deleteAll();
 		
 		User maria = new User(null, "Maria Brown", "maria@gmail.com");
 		User alex = new User(null, "Alex Green", "alex@gmail.com");
 		User bob = new User(null, "Bob Grey", "bob@gmail.com");
 		
-		repository.saveAll(Arrays.asList(maria, alex, bob));
+		userRepository.saveAll(Arrays.asList(maria, alex, bob));
 		
 		Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem", "Vou viajar para Sampa. Abraços!!!", new AuthorDTO(maria));
 		Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia", "Acordei feliz hoje", new AuthorDTO(maria));
 
 		postRepository.saveAll(Arrays.asList(post1, post2));
+		
+		maria.getPosts().addAll(Arrays.asList(post1, post2));
+		
+		userRepository.save(maria);
 	}
 
 }
