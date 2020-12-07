89c9ae67dd57e10cb0569b63bd8f2d790c79756f
Fabiano Guimarães <fabio.dub@outlook.com>
2020-12-07 18:26:51 -0300

    deletando usuario

--- src/main/java/com/soft/service/UserService.java
+++ src/main/java/com/soft/service/UserService.java
@@ -33,6 +33,11 @@ public class UserService {
 		return repository.insert(obj);
 	}
 	
+	public void delete(String id) {
+		findById(id);
+		repository.deleteById(id);
+	}
+	
 	public User fromDTO(UserDTO objDTO) {
 		return new User(null, objDTO.getNome(), objDTO.getEmail());
 	}
