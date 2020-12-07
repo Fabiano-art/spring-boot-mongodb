c02abb2bdcc6f54cb4425016112174e1da795995
Fabiano Guimarães <fabio.dub@outlook.com>
2020-12-07 20:31:24 -0300

    obter post pode id

---
+++ src/main/java/com/soft/service/PostService.java
@@ -0,0 +1,25 @@
+package com.soft.service;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.soft.domain.Post;
+import com.soft.repository.PostRepository;
+import com.soft.service.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+
+	@Autowired
+	private PostRepository repository;
+
+	public Post findById(String id) {
+
+		Optional<Post> obj = repository.findById(id);
+
+		return obj.orElseThrow(() -> new ObjectNotFoundException("The object requested was not found"));
+
+	}
+}
