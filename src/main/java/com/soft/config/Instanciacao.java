08a49d4f7ae934723f2222457e9aaf0d842e40b5
Fabiano Guimarães <fabio.dub@outlook.com>
2020-12-07 19:38:13 -0300

    entidade post

--- src/main/java/com/soft/config/Instanciacao.java
+++ src/main/java/com/soft/config/Instanciacao.java
@@ -1,12 +1,16 @@
 package com.soft.config;
 
+import java.text.SimpleDateFormat;
 import java.util.Arrays;
+import java.util.TimeZone;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 
+import com.soft.domain.Post;
 import com.soft.domain.User;
+import com.soft.repository.PostRepository;
 import com.soft.repository.UserRepository;
 
 @Configuration
@@ -15,16 +19,27 @@ public class Instanciacao implements CommandLineRunner{
 	@Autowired
 	private UserRepository repository;
 	
+	@Autowired
+	private PostRepository postRepository;
+	
 	@Override
 	public void run(String... args) throws Exception {
 		
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+		sdf.setTimeZone(TimeZone.getTimeZone("GMT"));
+		
 		repository.deleteAll();
+		postRepository.deleteAll();
 		
 		User maria = new User(null, "Maria Brown", "maria@gmail.com");
 		User alex = new User(null, "Alex Green", "alex@gmail.com");
 		User bob = new User(null, "Bob Grey", "bob@gmail.com");
 		
+		Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem", "Vou viajar para Sampa. Abraços!!!", maria);
+		Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia", "Acordei feliz hoje", maria);
+		
 		repository.saveAll(Arrays.asList(maria, alex, bob));
+		postRepository.saveAll(Arrays.asList(post1, post2));
 	}
 
 }
---
+++ src/main/java/com/soft/repository/PostRepository.java
@@ -0,0 +1,11 @@
+package com.soft.repository;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import com.soft.domain.Post;
+
+@Repository
+public interface PostRepository extends MongoRepository<Post, String>{
+
+}
