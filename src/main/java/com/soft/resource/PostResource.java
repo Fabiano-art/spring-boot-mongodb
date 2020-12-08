f8ba81935a66ed8c65b1b65630283e02a40ffef4
Fabiano Guimarães <fabio.dub@outlook.com>
2020-12-07 21:58:25 -0300

    Consulta com query method

--- src/main/java/com/soft/resource/PostResource.java
+++ src/main/java/com/soft/resource/PostResource.java
@@ -1,13 +1,17 @@
 package com.soft.resource;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.soft.domain.Post;
+import com.soft.resource.util.URL;
 import com.soft.service.PostService;
 
 @RestController
@@ -21,6 +25,14 @@ public class PostResource {
 	public ResponseEntity<Post> findById(@PathVariable String id){
 		Post post = service.findById(id);
 		return ResponseEntity.ok().body(post);
+	}
+	
+	@RequestMapping(value = "/titlesearch", method = RequestMethod.GET)
+	public ResponseEntity<List<Post>> findByTitle(@RequestParam(value = "text", defaultValue="") String text){
+		text = URL.decodeParam(text);
+		List<Post> list = service.findByTitle(text);
+		
+		return ResponseEntity.ok().body(list);
 	}
 	
 }
--- src/main/java/com/soft/service/PostService.java
+++ src/main/java/com/soft/service/PostService.java
@@ -1,5 +1,6 @@
 package com.soft.service;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -21,5 +22,9 @@ public class PostService {
 
 		return obj.orElseThrow(() -> new ObjectNotFoundException("The object requested was not found"));
 
+	}
+	
+	public List<Post> findByTitle(String title) {
+		return repository.findByTitleContainingIgnoreCase(title);
 	}
 }
