467da898ddb2e9c4da6e94eb31a2c2b9b2d3439f
Fabiano Guimarães <fabio.dub@outlook.com>
2020-12-07 22:06:11 -0300

    Consulta com @Query

--- src/main/java/com/soft/repository/PostRepository.java
+++ src/main/java/com/soft/repository/PostRepository.java
@@ -3,6 +3,7 @@ package com.soft.repository;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.soft.domain.Post;
@@ -11,5 +12,8 @@ import com.soft.domain.Post;
 public interface PostRepository extends MongoRepository<Post, String>{
 	
 	List<Post> findByTitleContainingIgnoreCase(String string);
+	
+	@Query("{ 'title': { $regex: ?0, $options: 'i' } }")
+	List<Post> findByTitle(String string);
 	
 }
--- src/main/java/com/soft/service/PostService.java
+++ src/main/java/com/soft/service/PostService.java
@@ -25,6 +25,6 @@ public class PostService {
 	}
 	
 	public List<Post> findByTitle(String title) {
-		return repository.findByTitleContainingIgnoreCase(title);
+		return repository.findByTitle(title);
 	}
 }
